281ce99389388a27f3407a2cd5bf723be14d64fd
Abhinandan-Pal <abhinandan.mike123@gmail.com>
2020-03-16 14:08:41 +0530

    basic plot

--- COVID-19.py
+++ COVID-19.py
@@ -20,5 +20,38 @@ conf = pd.read_csv('time_series-ncov-Confirmed.csv')[1:]
 dead = pd.read_csv('time_series-ncov-Deaths.csv')[1:]
 rec = pd.read_csv('time_series-ncov-Recovered.csv')[1:]
 
-conf.Lat.describe()
+
+############################################################################################################################
+#ANALYSING CONFIRMED CASES
+############################################################################################################################
+conf.Value = conf.Value.astype(float)
+conf.Long = conf.Long.astype(float)
 conf.Lat = conf.Lat.astype(float)
+conf_mod = conf[(conf['Value']>1)]
+#********************************************
+points = plt.scatter(conf_mod.Long,conf_mod.Lat, c=conf_mod.Value, lw=0)
+plt.colorbar(points)
+plt.show()
+#********************************************
+    #-----------------------------
+    #LATITUDE
+    #-----------------------------
+conf.Lat.describe()
+sns.distplot(conf.Lat)
+conf.Lat.value_counts()
+pd.crosstab(conf.Lat, columns = 'count' , normalize = True)
+sns.countplot(conf.Lat)
+sns.boxplot( x = 'Lat' , y = 'Value', data = conf)
+plt.hist(conf.Lat ,conf.Value, bin = 10)
+# Most confirmed cases between Latitude 20 to 60
+plt.scatter(conf.Long,conf.Lat)
+    #-----------------------------
+    #VALUES
+    #-----------------------------
+conf.Lat.describe()
+sns.distplot(conf.Lat)
+conf.Lat.value_counts()
+pd.crosstab(conf.Lat, columns = 'count' , normalize = True)
+sns.countplot(conf.Lat)
+sns.boxplot( x = 'Lat' , y = 'Value', data = conf)
+plt.hist(conf.Lat ,conf.Value, bin = 10)
